8df1f46dbff69d0c0475f1359d603b669ba5cbff
Prabhakaran Jeyamohan <prabha.jeyamohan@gmail.com>
2022-05-30 18:52:27 -0400

    Home page added

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,24 +4,30 @@ import {Link} from "react-router-dom";
 import BannerImage from "../assets/pics/splashpg_v1.jpg";
 import { BiRegistered } from "react-icons/bi";
 
+
 function Home() {
   return (
-    <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
+    <div className="homepage" style={{ backgroundImage: `url(${BannerImage})` }}>
       <div className="headerContainer">
-        <h1> 
+      <h1> 
           OVERWATCH 
           <sup className="registered-icon">
               <BiRegistered />
           </sup>
         </h1>
+        <br></br>
         <p> 5th Generation</p>
-        <Link to="/solution">
-          <button> Learn More </button>
+        <Link to="/Solution">
+          <button> Learn more </button>
         </Link>
+      </div>
+      <div className = "bottom-text">
+        ...a new era in autonomous warfare
       </div>
     </div>
   );
 }
+
 
 export default Home
 
